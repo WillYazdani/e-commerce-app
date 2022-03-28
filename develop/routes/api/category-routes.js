31adaa72d52e8870305d210b56c05cb37cdd584d
Will Yazdani <yazd0006@gmail.com>
2022-03-27 20:46:28 -0500

    category-routes added

--- develop/routes/api/category-routes.js
+++ develop/routes/api/category-routes.js
@@ -5,24 +5,53 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: [{
+      model: Product,
+      attributes: ['product_name', 'id', 'price', 'stock'],
+    }]
   // be sure to include its associated Products
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      attributes: ['product_name', 'id', 'price', 'stock'],
+    }]
   // be sure to include its associated Products
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  );
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
